27d80aab009938624084e95ef78ef8e6e4fae877
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2024-12-23 14:37:48 -0300

    fix(demo): improve dark theme support in demos

--- src/test/java/com/flowingcode/vaadin/addons/extendedlogin/LoginLayoutDemo.java
+++ src/test/java/com/flowingcode/vaadin/addons/extendedlogin/LoginLayoutDemo.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,7 @@
 package com.flowingcode.vaadin.addons.extendedlogin;
 
 import com.flowingcode.vaadin.addons.demo.DemoSource;
-import com.vaadin.flow.component.dependency.CssImport;
+import com.flowingcode.vaadin.addons.demo.ThemeChangeObserver;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.IFrame;
 import com.vaadin.flow.router.PageTitle;
@@ -31,14 +31,23 @@ import com.vaadin.flow.router.Route;
 @PageTitle("Login Layout Demo")
 @SuppressWarnings("serial")
 @Route(value = "extended-login/login-layout-demo", layout = ExtendedLoginDemoView.class)
-public class LoginLayoutDemo extends Div {
+public class LoginLayoutDemo extends Div implements ThemeChangeObserver {
+
+  private IFrame iframe;
 
   public LoginLayoutDemo() {
     setClassName("wrap-iframe");
-    IFrame iframe = new IFrame("/extended-login/login-layout");
+    iframe = new IFrame("/extended-login/login-layout");
     iframe.setClassName("frame");
     iframe.setSizeFull();
     iframe.getElement().setAttribute("frameBorder", "0");
     add(iframe);
   }
+
+  @Override
+  public void onThemeChange(String themeName) {
+    iframe.getElement().executeJs(
+        "this.contentWindow.document.querySelector('html').setAttribute('theme', $0)", themeName);
+  }
+
 }
